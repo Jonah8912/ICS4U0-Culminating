64e15ef6d91eaf8d03c5306dd203b30123964762
Jonah Perez <427JonahPerez@gmail.com>
2025-01-20 20:31:29 -0500

    Initial commit

---
+++ src/main/java/com/stats/TopBallerStats/TopBallerStatsApplication.java
@@ -0,0 +1,13 @@
+package com.stats.TopBallerStats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopBallerStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopBallerStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stats/TopBallerStats/TopBallerStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stats.TopBallerStats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopBallerStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
